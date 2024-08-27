8941bf553afbebc23c5c4e0c8286731808f5b8f0
hj0210 <cent2100@naver.com>
2024-08-27 17:46:58 +0900

    init

---
+++ src/main/java/com/ocs/azureDemo/web/OllehController.java
@@ -0,0 +1,15 @@
+package com.ocs.azureDemo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class OllehController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello, Azure with @RequestMapping!";
+    }
+}

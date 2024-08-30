0505b41ac8ed502fe5f22ca7f9245a7347072bee
hj0210 <cent2100@naver.com>
2024-08-30 09:08:23 +0900

    added /home/good url

--- src/main/java/com/ocs/azureDemo/web/OllehController.java
+++ src/main/java/com/ocs/azureDemo/web/OllehController.java
@@ -4,6 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
 @RequestMapping("/home")
 public class OllehController {
@@ -12,4 +19,18 @@ public class OllehController {
     public String hello() {
         return "Hello, Azure with ktds OCS!";
     }
+
+    @GetMapping("/good")
+    public Map<String, String> good() {
+        Map<String, String> response = new HashMap<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        response.put("message", "Hello, Azure with ktds OCS!");
+        response.put("timestamp", now.format(formatter));
+        
+        return response;
+    }
 }
+
+
